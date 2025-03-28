f28087e49d98cf7b091dbf172bb2b1bd6d82bc86
Mario <cmuoz.altimetrik@ancestry.com>
2025-03-28 17:01:32 -0500

    Add CurrentRide and Map Views

---
+++ Podfile
@@ -0,0 +1,20 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'Tekton' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Tekton
+  pod 'GoogleMaps'
+
+  target 'TektonTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'TektonUITests' do
+    # Pods for testing
+  end
+
+end
--- Tekton/TektonApp.swift
+++ Tekton/TektonApp.swift
@@ -5,15 +5,22 @@
 //  Created by Carlos Mario Muñoz on 28/03/25.
 //
 
+import GoogleMaps
 import SwiftUI
 
 @main
 struct TektonApp: App {
     @AppStorage("hasSeenOnboarding") private var hasSeenOnboarding: Bool = false
+    
+    init() {
+        GMSServices.provideAPIKey("AIzaSyDb_qgq7Pj_IQcYWnbiMAaW_KvBaKZ0PX4")
+    }
+    
+    
     var body: some Scene {
         WindowGroup {
-            if !hasSeenOnboarding {
-                ContentView()
+            if hasSeenOnboarding {
+                CurrentRideView()
             } else {
                 OnboardingView()
             }
